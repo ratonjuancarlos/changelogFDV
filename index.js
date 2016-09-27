0d5e42ced87ff4fa23ffef22be0b335f8745d79d
Pablo Hiyano <pablo.hiyano@fdvsolutions.com>
2016-09-27 15:00:55 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const fse = require('fs-extra');
+const fs = require('fs');
+const exec = require('child_process').exec;
+const _ = require('lodash');
+const changeCase = require('change-case');
+
+const cmd =  require('./lib/gitLog');
+const updateFields =  require('./lib/updateFields');
+const commitItem =  require('./lib/commitItem');
+
+
+
+
+exports.generateChangeLog = function() {
+    let result = {};
+    stream = fse.createOutputStream('CHANGELOG.html');
+
+    exec(cmd, function(error, stdout, stderr) {
+        stdout = stdout.substring(0, stdout.length - 1);
+        stdout = '[' + stdout + ']';
+        result = JSON.parse(stdout);
+
+        stream.write('<!DOCTYPE html><html><header></header><body>');
+        stream.write('<h1>Intelligize changelog</h1>');
+
+
+        result = updateFields(result);
+
+
+
+        let resultByDate = _.groupBy(result, 'date');
+		
+		for (let dateCommit in resultByDate) {
+		    stream.write('<h2>' + dateCommit + '</h2>');
+
+		    if (resultByDate.hasOwnProperty(dateCommit)) {
+		        let byBranch = _.groupBy(resultByDate[dateCommit], 'branchType');
+		    
+		        for (let branchTypeCommit in byBranch) {
+		            stream.write('<h3>' + branchTypeCommit + '</h3>');
+		    
+		            byIssue = _.orderBy(byBranch[branchTypeCommit], ['issueClean'], ['desc']);
+
+        			stream.write('<ul>');
+		    
+		            for (let issue in byIssue) {
+		            	let commit = byIssue[issue]
+		                stream.write( commitItem(commit) );
+
+		            }
+        	
+        			stream.write('</ul>');
+		        }
+		    }
+		}
+
+        stream.write('</body></html>');
+        stream.end();
+    });
+}
